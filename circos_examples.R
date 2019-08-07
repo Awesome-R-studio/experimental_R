6854039f5bad27ac54cf58ad50962373e3dd1594
Martin Jakt <lmjakt@gmail.com>
2019-08-07 09:21:08 +0200

    Added supporting files and SQL statements for circos_examples.R

--- circos_examples.R
+++ circos_examples.R
@@ -29,6 +29,26 @@ plotCircular <- function( s1, s2, s1.chr, s2.chr, ortho.t, chr.sep=1e7, main='',
     drawRadialLabels( c.o, radius=label.radius, depth=label.depth, bg=c('blue', 'red'), fg='white'  )
 }
 
+## The above tables were created using the following sql queries
+gene.query <- 'SELECT a.stable_id as gene_id, b.display_label as gene, c.name as chr,
+       a.seq_region_start as start, a.seq_region_end as end, a.seq_region_strand as strand,
+       d.stable_id as transcript_id, e.stable_id as protein_id from
+       gene a
+       inner join xref b on a.display_xref_id=b.xref_id
+       inner join seq_region c on a.seq_region_id=c.seq_region_id
+       inner join coord_system f on c.coord_system_id = f.coord_system_id and f.rank=1
+       inner join transcript d on a.gene_id=d.gene_id
+       inner join translation e on e.transcript_id=d.transcript_id order by c.name, a.seq_region_start;'
+
+chr.query <- 'SELECT a.* from
+       seq_region a
+       inner join coord_system b on a.coord_system_id=b.coord_system_id and b.rank=1
+       order by a.length desc;'
+
+
+## mRNA is used by make.orthologue.table
+mRNA <- read.table('mRNA.txt', sep="\t", stringsAsFactors=FALSE )
+
 gadMor2.chr <- read.table("gadMor2.lengths", header=FALSE, sep="\t", stringsAsFactors=FALSE)
 colnames(gadMor2.chr) <- c('name', 'length')
 gadMor2.chr <- cbind( gadMor2.chr, 'off'=(c(0, cumsum(gadMor2.chr[,'length']))[1:nrow(gadMor2.chr)]) )
